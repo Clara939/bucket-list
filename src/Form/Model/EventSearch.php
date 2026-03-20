0045cdd582feaf1557e3cb9c7b61790153bfc534
chuet2025 <clara.huet2025@campus_eni.fr>
2026-03-20 09:06:03 +0100

    api page events

---
+++ src/Form/Model/EventSearch.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form\Model;
+
+class EventSearch
+{
+    private ?string $city = null;
+
+    private ?\DateTime $startDate = null;
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(?string $city): void
+    {
+        $this->city = $city;
+    }
+
+    public function getStartDate(): ?\DateTime
+    {
+        return $this->startDate;
+    }
+
+    public function setStartDate(?\DateTime $startDate): void
+    {
+        $this->startDate = $startDate;
+    }
+
+
+
+}
---
+++ src/Services/EventService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Services;
+
+use App\Form\Model\EventSearch;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class EventService
+{
+
+    private readonly string $BASE_URL;
+
+    public function __construct(private HttpClientInterface $httpClient)
+    {
+        $this->BASE_URL = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=evenements-publics-openagenda";
+    }
+
+
+    public function getDataFromApi(EventSearch $eventSearch)
+    {
+        $url = $this->BASE_URL;
+
+        if ($eventSearch->getCity()) {
+            $url .= '&refine.location_city=' . $eventSearch->getCity();
+        }
+
+        if ($eventSearch->getStartDate()) {
+            $url .= '&refine.firstdate_begin=' . $eventSearch->getStartDate()->format('Y-m-d');
+        }
+
+        $data = $this->httpClient->request('GET', $url)->toArray();
+
+        return $data['records'];
+    }
+
+
+}
